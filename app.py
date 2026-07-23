699281e7090ccbda42210c699450d373698c8885
Hacker9826 <amanbijore2004@gmail.com>
2026-07-23 23:12:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -293,4 +293,6 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port, debug=True)
